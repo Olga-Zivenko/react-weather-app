3d89e7022bf52511b058ae5ab5ab213fbe61c4b8
Olga-Zivenko <108621714+Olga-Zivenko@users.noreply.github.com>
2022-09-23 09:29:21 +0200

    add link to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      This project was coded by Olga Zivenko and is 
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Olga-Zivenko/react-weather-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
-      </header>
+          open-soursed on GitHub</a>
+     
     </div>
   );
 }
